3b74d40a063a091f5430ae62e698bff3ba243e73
Suprokas Midde <suprokas2004midde@gmail.com>
2026-04-17 21:05:18 +0530

    Initial setup with navigation bar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { Routes,Route } from 'react-router';
+import Home from './pages/Home.jsx';
+import Cart from './pages/Cart.jsx';
+import Collection from './pages/Collection.jsx';
+import Login from './pages/Login.jsx'; 
+import ProductDetail from './pages/ProductDetail.jsx';
+import Orders from './pages/Orders.jsx';
+import Contacts from './pages/Contacts.jsx';
+import PlaceOrder from './pages/PlaceOrder.jsx';
+import About from './pages/About.jsx';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+
+  return (
+    <div className='px-4'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/cart' element={ <Cart/> } />
+        <Route path='/collection'  element={ <Collection/> } />
+        <Route path='/login' element={ <Login/> } />
+        <Route path='/product/:productID' element={ <ProductDetail/> } />
+        <Route path='orders' element={ <Orders/> } />
+        <Route path='contact' element={ <Contacts/> } />
+        <Route path='place-order' element={ <PlaceOrder/> } />
+        <Route path='about' element={<About/>} />
+      </Routes>      
+    </div>
+  );
+}
+
+export default App
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router";
+import { IoSearch } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { BsCartCheck } from "react-icons/bs";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoArrowBack } from "react-icons/io5";
+import logo from "../assets/logo.png";
+
+const Navbar = () => {
+
+    const [visible,setvisible] = useState(false);
+
+  return (
+
+    <div className="flex items-center justify-between py-5 font-medium">
+
+      <img src={logo} alt="Brand_logo" className="w-50" />
+
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col gap-1 items-center">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+        <NavLink to="/collection" className="flex flex-col gap-1 items-center">
+          <p>COLLECTION</p>
+          <hr className="w-2/4 border-none bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+        <NavLink to="/about" className="flex flex-col gap-1 items-center">
+          <p>ABOUT</p>
+          <hr className="w-2/4 border-none bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+        <NavLink to="/contact" className="flex flex-col gap-1 items-center">
+          <p>CONTACTS</p>
+          <hr className="w-2/4 border-none bg-gray-700 h-[1.5px] hidden" />
+        </NavLink>
+      </ul>
+
+
+      <div className="flex flex-row gap-6">
+        <div>
+          <IoSearch className="cursor-pointer" />
+        </div>
+        <div className="group relative">
+          <CgProfile className="cursor-pointer" />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 px-5 py-3 bg-slate-100 text-gray-500">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+        <div className="relative">
+            <Link to='/'>
+                <BsCartCheck className="w-5 min-w-5"/>
+                <p className="absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">9</p>
+            </Link>
+        </div>
+        <HiMenuAlt3 onClick={()=>setvisible(true)} className="cursor-pointer sm:hidden"/>
+      </div>
+      
+      {/*This is for drop down menu */}
+
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible?'w-full': 'w-0'}`}>
+            <div className="flex flex-col text-gray-600">
+                <div onClick={()=> setvisible(false)} className="flex items-center gap-4 p-3 ">
+                    <IoArrowBack />
+                    <p>Back</p>
+                </div>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=>setvisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+            </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
